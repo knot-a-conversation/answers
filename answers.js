5b3171b9f24501901fd1596c7d3c67e6aa1678ad
ambikajo <ambikajoshi@gmail.com>
2022-04-26 01:58:52 +0530

    answers will appear on 27th

--- answers.js
+++ answers.js
@@ -23,7 +23,7 @@ async function GPTAnswer(){
        questionprompt = data.question;
        if(data.startedAt){
 
-         console.log(data.startedAt, new Date(data.startedAt*1000))
+         console.log(data.startedAt, new Date(data.startedAt))
        }
        spl = answerGPT.split(". ");
           if(spl.length > 1){
@@ -35,7 +35,7 @@ async function GPTAnswer(){
             finalAns = answerGPT;
           }
           
-      //  console.log(finalAns)
+     
           // console.log("new key should be: " + data.key)
           // console.log("Previous Post ID: " + prevChildKey);
           const ques = createP("Question: ").addClass("qna");
@@ -45,32 +45,50 @@ async function GPTAnswer(){
           var li_a = createP(finalAns).addClass("dba");  
 
           if(data.startedAt){
-            timestamp = data.startedAt
-            GPTdate = new Date(timestamp)
-            var year = GPTdate.getFullYear();
-            var month = GPTdate.getMonth() + 1;
-            var day = GPTdate.getDate();
-            var hours = GPTdate.getHours();
-            var minutes = GPTdate.getMinutes();
+            // timestamp = data.startedAt
+            // GPTdate = new Date(timestamp)
+            // var year = GPTdate.getFullYear();
+            // var month = GPTdate.getMonth() + 1;
+            // var day = GPTdate.getDate();
+            // var hours = GPTdate.getHours();
+            // var minutes = GPTdate.getMinutes();
 
-            formattedDate = year + "-" + month + "-" + day + " " + hours + ":" + minutes;
+            // formattedDate = year + "-" + month + "-" + day;
             //var date = new Date(timestamp * 1000);
             // formattedDate = addDate.getFullYear()
-            const dateAnswered = createP("Date answered: ").addClass("qna-date");
-            var li_date = createP(formattedDate).addClass("dbdate")
-            date_set = dateAnswered.parent(setDiv) 
-            date_set2 = li_date.parent(setDiv) 
+            // const dateAnswered = createP("Date answered: ").addClass("qna-date");
+            // var li_date = createP(formattedDate).addClass("dbdate")
+            // date_set = dateAnswered.parent(setDiv) 
+            // date_set2 = li_date.parent(setDiv) 
+
+          }
+          //timestamp is 2022-04-27 00:00:00
+          if(data.startedAt > 1650997800){ 
+
+            ques.parent("iaf")
+            li_q.parent("iaf")
+            answers.parent("iaf")
+            li_a.parent("iaf")
+            // dateAnswered.parent("iaf")
+            // li_date.parent("iaf")
+           
+          } else {
+            ques.parent("vorspiel")
+            li_q.parent("vorspiel")
+            answers.parent("vorspiel")
+            li_a.parent("vorspiel")
+    
           }
 ;  
 
-        setDiv = createDiv().addClass("set-order")
-        q_set = ques.parent(setDiv);
-        q_set2 =  li_q.parent(setDiv);
-        a_set= answers.parent(setDiv);
-        a_set2=  li_a.parent(setDiv);
+        // setDiv = createDiv().addClass("set-order")
+        // q_set = ques.parent(setDiv);
+        // q_set2 =  li_q.parent(setDiv);
+        // a_set= answers.parent(setDiv);
+        // a_set2=  li_a.parent(setDiv);
           
         // reverseSet = createDiv().addId("reverse-order")
-        setDiv.parent("reverse-order")
+        // setDiv.parent("reverse-order")
      
   });
   };
